a87cc5bfa2b07b3b04808d284e9d2eabcf88d438
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2022-01-13 21:20:26 +0330

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import store from './redux/store';
 
-import Home from './page/home';
+import './index.css';
+import App from './App';
 
-function App()
-{
-    return (
-        <>
-            <React.StrictMode>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={ <Home /> } />
-                    </Routes>
-                </BrowserRouter>
-            </React.StrictMode>
-        </>);
-}
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('App'));
+ReactDOM.render(
+    <Provider store={store}>
+        <React.StrictMode>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </React.StrictMode>
+    </Provider>,
+    document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/pages/home/home.Component.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './home.Styles.scss';
+
+const Home = () =>
+{
+    return (
+        <>
+            Main Content
+        </>
+    );
+};
+
+export default Home;
