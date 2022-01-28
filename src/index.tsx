e2ea3c364322c4b4d5ffd541febde9c3e01d0a1e
Redeemer <dev.khazaee@gmail.com>
2022-01-28 12:07:47 +0330

    Revert "update"
    This reverts commit a87cc5bfa2b07b3b04808d284e9d2eabcf88d438.

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 
-import store from './redux/store';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import './index.css';
-import App from './App';
+import Home from './page/home';
 
-import reportWebVitals from './reportWebVitals';
+function App()
+{
+    return (
+        <>
+            <React.StrictMode>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={ <Home /> } />
+                    </Routes>
+                </BrowserRouter>
+            </React.StrictMode>
+        </>);
+}
 
-ReactDOM.render(
-    <Provider store={store}>
-        <React.StrictMode>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
-        </React.StrictMode>
-    </Provider>,
-    document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('App'));
