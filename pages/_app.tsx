9ab09049cfb0c370031a74950191fcd3b6fae4b4
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2022-05-06 13:36:15 +0430

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,7 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 
-<<<<<<< HEAD
-import Nav from '../components/layouts/nav/nav.component';
-=======
 import Nav from '../components/nav/nav.component';
->>>>>>> 8f65f1c (Navbar)
 
 function MyApp({Component, pageProps}: AppProps)
 {
@@ -14,11 +10,7 @@ function MyApp({Component, pageProps}: AppProps)
             <Nav />
             <Component {...pageProps} />
         </>
-<<<<<<< HEAD
-    );
-=======
     )
->>>>>>> 8f65f1c (Navbar)
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,11 @@
-
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from "next/image";
-
-import Main from "../components/layouts/main/main.component";
-
-import styles from '../styles/Home.module.scss';
-import Button from "../components/button/button.component";
-
 import type {NextPage} from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import classnames from 'classnames';
 
 import styles from '../styles/Home.module.scss'
-
-8f65f1c (Navbar)
-
 import Button from "../components/button/button.component";
-
-7671eb1 (update)
-
 import Link from "next/link";
- 3cf4e7c (Market)
 
 const Home: NextPage = () =>
 {
@@ -36,117 +19,6 @@ const Home: NextPage = () =>
                 <title>Guardian Community</title>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-
-
-            <Main>
-                <section className={styles.home}>
-                    <div className={styles.homeHeaderLeft}>
-                        <h1>
-                            Buy & sell <br />
-                            crypto in minutes
-                        </h1>
-                        <p>
-                            Trade Bitcoin, Ethereum, USDT, and the top altcoins on the legendary crypto asset exchange.
-                        </p>
-                        <Button name="Get started now" type="Primary" />
-                    </div>
-                    <span className={styles.homeHeaderRight}>
-                        <Image
-                            src="/static/images/content/cards.png"
-                            alt="Guardian Community"
-                            width={913}
-                            height={924}
-                            objectFit="none"
-                        />
-                    </span>
-
-                    <div className={styles.homeCards}>
-                        <div className={styles.homeCard}>
-                            <span className={styles.homeCardIcon}>
-                                <Image
-                                    src="/static/images/content/currency/bitcoin.svg"
-                                    alt="Guardian Community"
-                                    width={40}
-                                    height={40}
-                                />
-                            </span>
-
-                            <div className={styles.homeCardDetails}>
-                                <div className={styles.homeCardDetailsInfo}>
-                                    <span className={styles.homeCardDetailsInfoSubTitle}>BTC/USDT</span>
-                                    <span className={styles.homeCardDetailsInfoNegative}>-0.79%</span>
-                                </div>
-
-                                <div className={styles.homeCardDetailsPrice}>36,641.20</div>
-                                <div className={styles.homeCardDetailsMoney}>36,641.20</div>
-                            </div>
-                        </div>
-                        <div className={styles.homeCard}>
-                            <span className={styles.homeCardIcon}>
-                                <Image
-                                    src="/static/images/content/currency/chainlink.svg"
-                                    alt="Guardian Community"
-                                    width={40}
-                                    height={40}
-                                />
-                            </span>
-
-                            <div className={styles.homeCardDetails}>
-                                <div className={styles.homeCardDetailsInfo}>
-                                    <span className={styles.homeCardDetailsInfoSubTitle}>BTC/USDT</span>
-                                    <span className={styles.homeCardDetailsInfoPositive}>-0.79%</span>
-                                </div>
-
-                                <div className={styles.homeCardDetailsPrice}>36,641.20</div>
-                                <div className={styles.homeCardDetailsMoney}>36,641.20</div>
-                            </div>
-                        </div>
-                        <div className={styles.homeCard}>
-                            <span className={styles.homeCardIcon}>
-                                <Image
-                                    src="/static/images/content/currency/monero.svg"
-                                    alt="Guardian Community"
-                                    width={40}
-                                    height={40}
-                                />
-                            </span>
-
-                            <div className={styles.homeCardDetails}>
-                                <div className={styles.homeCardDetailsInfo}>
-                                    <span className={styles.homeCardDetailsInfoSubTitle}>BTC/USDT</span>
-                                    <span className={styles.homeCardDetailsInfoPositive}>-0.79%</span>
-                                </div>
-
-                                <div className={styles.homeCardDetailsPrice}>36,641.20</div>
-                                <div className={styles.homeCardDetailsMoney}>36,641.20</div>
-                            </div>
-                        </div>
-                        <div className={styles.homeCard}>
-                            <span className={styles.homeCardIcon}>
-                                <Image
-                                    src="/static/images/content/currency/maid.svg"
-                                    alt="Guardian Community"
-                                    width={40}
-                                    height={40}
-                                />
-                            </span>
-
-                            <div className={styles.homeCardDetails}>
-                                <div className={styles.homeCardDetailsInfo}>
-                                    <span className={styles.homeCardDetailsInfoSubTitle}>BTC/USDT</span>
-                                    <span className={styles.homeCardDetailsInfoPositive}>-0.79%</span>
-                                </div>
-
-                                <div className={styles.homeCardDetailsPrice}>36,641.20</div>
-                                <div className={styles.homeCardDetailsMoney}>36,641.20</div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </Main>
-
-8f65f1c (Navbar)
-
 
             <main className="container">
                 <section className={styles.sectionStart}>
@@ -423,7 +295,6 @@ const Home: NextPage = () =>
                     </div>
                 </section>
             </main>
-7671eb1 (update)
         </>
     )
 }
